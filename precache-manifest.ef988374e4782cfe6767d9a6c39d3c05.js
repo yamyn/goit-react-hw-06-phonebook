025f76efaac381906c272c7dc273a2a1ff0478eb
ya_myn <yaroslav.mynchenko@onix-systems.com>
2020-05-12 12:23:34 +0300

    Updates

---
+++ precache-manifest.ef988374e4782cfe6767d9a6c39d3c05.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e739580864ac5fbb2bd5ea46b4ce2d",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "3f7ac1b9c37860ed2049",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.51e6f103.chunk.css"
+  },
+  {
+    "revision": "4b958b5eb9522da6a47e",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.b9be9fbc.chunk.css"
+  },
+  {
+    "revision": "3f7ac1b9c37860ed2049",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0f96c264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0f96c264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b958b5eb9522da6a47e",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.5f4f7a2e.chunk.js"
+  },
+  {
+    "revision": "0c1b11dc9b816a72f018",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.ee390fb5.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/goit-react-hw-06-phonebook/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/goit-react-hw-06-phonebook/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/goit-react-hw-06-phonebook/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/goit-react-hw-06-phonebook/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
