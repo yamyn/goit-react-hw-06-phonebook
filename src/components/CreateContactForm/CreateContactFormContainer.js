368a36354bb63109ff49c85a10d3adf22e971d1a
ya_myn <yaroslav.mynchenko@onix-systems.com>
2020-05-23 22:23:35 +0300

    fix(remove import): remove left import im formContainer

--- src/components/CreateContactForm/CreateContactFormContainer.js
+++ src/components/CreateContactForm/CreateContactFormContainer.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import * as contactsActions from '../../redux/contacts/contactsActions';
 import CreateContactForm from './CreateContactForm';
-import toAddId from '../../helpers/toAddId';
 
 const mapDispatchToProps = dispatch => ({
     onAddContact: contact => dispatch(contactsActions.addContact(contact)),
