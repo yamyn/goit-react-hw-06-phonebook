af18deb97bf82ed89cafdf7441789b64e60cc4a0
ya_myn <yaroslav.mynchenko@onix-systems.com>
2020-06-09 17:04:14 +0300

    refactor(remove left): remove old code, that was comented

--- src/redux/contacts/contactsReducer.js
+++ src/redux/contacts/contactsReducer.js
@@ -1,4 +1,3 @@
-// import { combineReducers } from 'redux';
 import { createReducer, combineReducers } from '@reduxjs/toolkit';
 import {
     deleteContact,
@@ -7,28 +6,7 @@ import {
     addContact,
 } from './contactsActions';
 
-// import Type from './contactsTypes';
 import * as ls from '../../helpers/saveLocalStorage';
-// import {
-//     toGetContacts,
-//     toAddContacts,
-//     toDeleteContacts,
-// } from '../../helpers/saveLocalStorage';
-
-// const contactsReducer = (state = [], action) => {
-//     switch (action.type) {
-//         case Type.LOADCONTACTS:
-//             return toGetContacts();
-
-//         case Type.ADDCONTACT:
-//             return toAddContacts(state, action.payload.contact);
-
-//         case Type.DELETECONTACT:
-//             return toDeleteContacts(state, action.payload.id);
-//         default:
-//             return state;
-//     }
-// };
 
 const contactsReducer = createReducer([], {
     [loadContacts]: () => ls.toGetContacts(),
@@ -37,15 +15,6 @@ const contactsReducer = createReducer([], {
     [deleteContact]: (state, action) =>
         ls.toDeleteContacts(state, action.payload.id),
 });
-
-// const filterReducer = (state = '', action) => {
-//     switch (action.type) {
-//         case Type.CHANGEFILTER:
-//             return action.payload.filter;
-//         default:
-//             return state;
-//     }
-// };
 
 const filterReducer = createReducer('', {
     [changeFilter]: (state, action) => action.payload.filter,
